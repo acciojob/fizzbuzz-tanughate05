a869bd3e4ba7279fbc29e5bcc468c21fac6eb376
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-08-03 18:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const FizzBuzz2 = function() {
+	const arr = [];
+	arr.length = 20;
+	for(let i = 1; i <= arr.length; i++) {
+		arr[i-1] = ((i % 3) ? '' : 'Fizz') + ((i % 5) ? '' : 'Buzz') || i;
+	}
+	return arr.join(', ');
+};
+
+console.log(FizzBuzz2());
